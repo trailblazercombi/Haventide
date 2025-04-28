ed1f520c32c2b22f449d50b336fc83d2715b2f64
trailblazercombi <67699510+trailblazercombi@users.noreply.github.com>
2025-04-28 11:18:36 +0200

    Update DialogGenerics.kt
    Fix undesired tap indication

--- composeApp/src/commonMain/kotlin/app/trailblazercombi/haventide/game2/jetpack/universal/DialogGenerics.kt
+++ composeApp/src/commonMain/kotlin/app/trailblazercombi/haventide/game2/jetpack/universal/DialogGenerics.kt
@@ -5,11 +5,13 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import app.trailblazercombi.haventide.resources.Palette
@@ -30,7 +32,10 @@ fun DialogGenerics(
             modifier = modifier
                 .fillMaxSize()
                 .background(Palette.Abyss80)
-                .clickable {
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null
+                ) {
                     onDismissRequest()
                 }
         ) {}
